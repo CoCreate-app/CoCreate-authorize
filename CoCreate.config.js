005b82745002a2d9d33ca1e0625f50287db30572
frankpagan <frank@cocreate.app>
2023-05-31 22:32:46 +0000

    feat: Initial release. Compares data with authorized object and removes keys user does not have authorization to access.

---
+++ CoCreate.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    "config": {
+        "organization_id": "5ff747727005da1c272740ab",
+        "key": "2061acef-0451-4545-f754-60cf8160",
+        "host": "general.cocreate.app"
+    },
+    "sources": [
+        {
+            "collection": "files",
+            "document": {
+                "_id": "637ca55150234ef1671ce324",
+                "name": "index.html",
+                "path": "/docs/authorize/index.html",
+                "src": "{{./docs/index.html}}",
+                "hosts": [
+                    "*",
+                    "general.cocreate.app"
+                ],
+                "directory": "/docs/authorize",
+                "content-type": "text/html",
+                "public": "true",
+                "website_id": "5ffbceb7f11d2d00103c4535"
+            }
+        }
+    ]
+}
